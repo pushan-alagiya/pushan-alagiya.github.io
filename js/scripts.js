c907a00b2eebb9f81015e14f44f969c1b0d0cdf8
pushan <pushanalagiya@gmail.com>
2024-03-24 15:26:27 +0530

    Adding viewport functionality for cursor

--- js/scripts.js
+++ js/scripts.js
@@ -1,210 +1,235 @@
 let script = document.currentScript;
 
 window.addEventListener("DOMContentLoaded", () => {
-  let iDiv = document.createElement("div");
-  iDiv.id = "cursor";
-  if (script.getAttribute("difference") == "disable") {
-    iDiv.className = "mscursor-cursor";
-  } else {
-    iDiv.className = "mscursor-cursor mscursor-difference";
-  }
-  document.getElementsByTagName("body")[0].appendChild(iDiv);
-
+  let tglbtn = document.querySelector(".night-toggler");
+  const bodyElement = document.body;
   const profileImages = document.querySelectorAll(".noinverseimgages");
-  function isCursorOverProfileImage(e) {
-    for (let i = 0; i < profileImages.length; i++) {
-      const rect = profileImages[i].getBoundingClientRect();
-      if (
-        e.clientX >= rect.left &&
-        e.clientX <= rect.right &&
-        e.clientY >= rect.top &&
-        e.clientY <= rect.bottom
-      ) {
-        return true;
-      }
-    }
-    return false;
-  }
+  const otherElements = document.querySelectorAll(
+    "body *:not(.noinverseimages)"
+  );
 
-  let pauseAnimation = script.getAttribute("pause-animation");
-
-  let innerDiv = document.createElement("div");
-
-  if (script.getAttribute("color") !== null) {
-    iDiv.style.backgroundColor = script.getAttribute("color");
-  } else {
-    if (script.getAttribute("difference") == "disable") {
-      iDiv.style.backgroundColor = "black";
-    } else {
-      iDiv.style.backgroundColor = "white";
-    }
-  }
-
-  if (pauseAnimation !== null && pauseAnimation == "disable") {
-    if (script.getAttribute("circle-outline") == "disable") {
-      innerDiv.className = "mscursor-circle";
-    } else {
-      innerDiv.className = "mscursor-circle new";
-    }
-  } else {
-    if (script.getAttribute("circle-outline") == "disable") {
-      innerDiv.className = "mscursor-circle mscursor-border-transform";
-    } else {
-      innerDiv.className = "mscursor-circle new mscursor-border-transform";
-    }
-  }
-
-  iDiv.appendChild(innerDiv);
-
-  let size = Number(script.getAttribute("size")) || 30;
-
-  for (let i = 0; i < size; i++) {
-    let innerDiv = document.createElement("div");
-    if (pauseAnimation !== null && pauseAnimation == "disable") {
-      innerDiv.className = "mscursor-circle";
-    } else {
-      innerDiv.className = "mscursor-circle mscursor-border-transform";
-    }
-
-    if (script.getAttribute("color") !== null) {
-      innerDiv.style.backgroundColor = script.getAttribute("color");
-    } else {
-      if (script.getAttribute("difference") == "disable") {
-        innerDiv.style.backgroundColor = "black";
-      } else {
-        innerDiv.style.backgroundColor = "white";
-      }
-    }
-    iDiv.appendChild(innerDiv);
-  }
-
-  const coords = { x: 0, y: 0 };
-  let timeout;
-  const circles = document.querySelectorAll(".mscursor-circle");
-
-  const cursor = document.querySelector(".mscursor-cursor");
-
-  circles.forEach(function (circle, index) {
-    circle.x = 0;
-    circle.y = 0;
-    if (script.getAttribute("gradient") !== null) {
-      let colors = script.getAttribute("gradient").split(",");
-      circle.style.backgroundColor =
-        colors[Math.floor((index * colors.length) / circles.length)];
-
-      document.querySelector("div.new").border = `0.5px solid ${colors[0]}`;
-    }
+  tglbtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    bodyElement.classList.toggle("inverted");
+    otherElements.forEach((element) => {
+      element.style.filter = bodyElement.classList.contains("inverted")
+        ? "invert(100%)"
+        : "none";
+    });
+    profileImages.forEach((image) => {
+      image.style.filter = "none";
+    });
   });
 
-  const addclass = (e) => {
-    const isOverProfileImage = isCursorOverProfileImage(e);
-    // If cursor is over a profile image, apply different cursor behavior
-    if (!isOverProfileImage) {
-      iDiv.classList.add("mscursor-difference");
-
-      if (script.getAttribute("pause-animation") !== "disable") {
-        document.body.classList.remove("mscursor-nocursor");
-        if (script.getAttribute("circle-outline") !== "disable") {
-          document
-            .querySelector("div.new")
-            .classList.remove("mscursor-scale-outline");
-          document.querySelector("div.new").style.border = "";
-        }
-        document.querySelectorAll("div.mscursor-circle").forEach((element) => {
-          element.classList.remove("mscursor-scale");
-        });
-      }
-      coords.x = e.clientX;
-      coords.y = e.clientY;
+  if (window.innerWidth > 450) {
+    let iDiv = document.createElement("div");
+    iDiv.id = "cursor";
+    if (script.getAttribute("difference") == "disable") {
+      iDiv.className = "mscursor-cursor";
     } else {
-      iDiv.classList.remove("mscursor-difference");
-      coords.x = e.clientX;
-      coords.y = e.clientY;
+      iDiv.className = "mscursor-cursor mscursor-difference";
     }
-  };
+    document.getElementsByTagName("body")[0].appendChild(iDiv);
 
-  window.addEventListener("mousemove", (e) => addclass(e));
-  window.addEventListener("touchmove", (e) => addclass(e.touches[0]));
+    function isCursorOverProfileImage(e) {
+      for (let i = 0; i < profileImages.length; i++) {
+        const rect = profileImages[i].getBoundingClientRect();
+        if (
+          e.clientX >= rect.left &&
+          e.clientX <= rect.right &&
+          e.clientY >= rect.top &&
+          e.clientY <= rect.bottom
+        ) {
+          return true;
+        }
+      }
+      return false;
+    }
 
-  function animateCircles() {
-    let x = coords.x;
-    let y = coords.y;
+    let pauseAnimation = script.getAttribute("pause-animation");
 
-    cursor.style.top = x;
-    cursor.style.left = y;
+    let innerDiv = document.createElement("div");
+
+    if (script.getAttribute("color") !== null) {
+      iDiv.style.backgroundColor = script.getAttribute("color");
+    } else {
+      if (script.getAttribute("difference") == "disable") {
+        iDiv.style.backgroundColor = "black";
+      } else {
+        iDiv.style.backgroundColor = "white";
+      }
+    }
+
+    if (pauseAnimation !== null && pauseAnimation == "disable") {
+      if (script.getAttribute("circle-outline") == "disable") {
+        innerDiv.className = "mscursor-circle";
+      } else {
+        innerDiv.className = "mscursor-circle new";
+      }
+    } else {
+      if (script.getAttribute("circle-outline") == "disable") {
+        innerDiv.className = "mscursor-circle mscursor-border-transform";
+      } else {
+        innerDiv.className = "mscursor-circle new mscursor-border-transform";
+      }
+    }
+
+    iDiv.appendChild(innerDiv);
+
+    let size = Number(script.getAttribute("size")) || 30;
+
+    for (let i = 0; i < size; i++) {
+      let innerDiv = document.createElement("div");
+      if (pauseAnimation !== null && pauseAnimation == "disable") {
+        innerDiv.className = "mscursor-circle";
+      } else {
+        innerDiv.className = "mscursor-circle mscursor-border-transform";
+      }
+
+      if (script.getAttribute("color") !== null) {
+        innerDiv.style.backgroundColor = script.getAttribute("color");
+      } else {
+        if (script.getAttribute("difference") == "disable") {
+          innerDiv.style.backgroundColor = "black";
+        } else {
+          innerDiv.style.backgroundColor = "white";
+        }
+      }
+      iDiv.appendChild(innerDiv);
+    }
+
+    const coords = { x: 0, y: 0 };
+    let timeout;
+    const circles = document.querySelectorAll(".mscursor-circle");
+
+    const cursor = document.querySelector(".mscursor-cursor");
 
     circles.forEach(function (circle, index) {
-      circle.style.left = x - 12 + "px";
-      circle.style.top = y - 12 + "px";
+      circle.x = 0;
+      circle.y = 0;
+      if (script.getAttribute("gradient") !== null) {
+        let colors = script.getAttribute("gradient").split(",");
+        circle.style.backgroundColor =
+          colors[Math.floor((index * colors.length) / circles.length)];
 
-      circle.style.scale = (circles.length - index) / circles.length;
-
-      circle.x = x;
-      circle.y = y;
-
-      const nextCircle = circles[index + 1] || circles[0];
-      x += (nextCircle.x - x) * 0.3;
-      y += (nextCircle.y - y) * 0.3;
+        document.querySelector("div.new").border = `0.5px solid ${colors[0]}`;
+      }
     });
 
-    requestAnimationFrame(animateCircles);
-  }
-
-  animateCircles();
-
-  if (script.getAttribute("cursor") == "disable") {
-    document.body.classList.add("mscursor-nocursor");
-  }
-
-  if (script.getAttribute("pause-animation") !== "disable") {
-    const moove = () => {
-      clearTimeout(timeout);
-      timeout = setTimeout(() => {
-        document.body.classList.add("mscursor-nocursor");
-        if (script.getAttribute("mscursor-circle-outline") !== "disable") {
+    const addclass = (e) => {
+      const isOverProfileImage = isCursorOverProfileImage(e);
+      // if max width is 450 then dont app cursor else apply it
+      if (!isOverProfileImage) {
+        iDiv.classList.add("mscursor-difference");
+        if (script.getAttribute("pause-animation") !== "disable") {
+          document.body.classList.remove("mscursor-nocursor");
+          if (script.getAttribute("circle-outline") !== "disable") {
+            document
+              .querySelector("div.new")
+              .classList.remove("mscursor-scale-outline");
+            document.querySelector("div.new").style.border = "";
+          }
           document
-            .querySelector("div.new")
-            .classList.add("mscursor-scale-outline");
+            .querySelectorAll("div.mscursor-circle")
+            .forEach((element) => {
+              element.classList.remove("mscursor-scale");
+            });
+        }
+        coords.x = e.clientX;
+        coords.y = e.clientY;
+      } else {
+        // If cursor is over a profile image, apply different cursor behavior
+        iDiv.classList.remove("mscursor-difference");
+        coords.x = e.clientX;
+        coords.y = e.clientY;
+      }
+    };
 
-          if (script.getAttribute("color") !== null) {
-            if (script.getAttribute("color-outline") !== null) {
-              document.querySelector(
-                "div.new"
-              ).style.border = `0.5px solid ${script.getAttribute(
-                "color-outline"
-              )}`;
-            } else {
-              document.querySelector(
-                "div.new"
-              ).style.border = `0.5px solid ${script.getAttribute("color")}`;
-            }
-          } else {
-            if (script.getAttribute("color-outline") !== null) {
-              document.querySelector(
-                "div.new"
-              ).style.border = `0.5px solid ${script.getAttribute(
-                "color-outline"
-              )}`;
-            } else {
-              if (script.getAttribute("difference") == "disable") {
+    window.addEventListener("mousemove", (e) => addclass(e));
+    window.addEventListener("touchmove", (e) => addclass(e.touches[0]));
+
+    function animateCircles() {
+      let x = coords.x;
+      let y = coords.y;
+
+      cursor.style.top = x;
+      cursor.style.left = y;
+
+      circles.forEach(function (circle, index) {
+        circle.style.left = x - 12 + "px";
+        circle.style.top = y - 12 + "px";
+
+        circle.style.scale = (circles.length - index) / circles.length;
+
+        circle.x = x;
+        circle.y = y;
+
+        const nextCircle = circles[index + 1] || circles[0];
+        x += (nextCircle.x - x) * 0.3;
+        y += (nextCircle.y - y) * 0.3;
+      });
+
+      requestAnimationFrame(animateCircles);
+    }
+
+    animateCircles();
+
+    if (script.getAttribute("cursor") == "disable") {
+      document.body.classList.add("mscursor-nocursor");
+    }
+
+    if (script.getAttribute("pause-animation") !== "disable") {
+      const moove = () => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+          document.body.classList.add("mscursor-nocursor");
+          if (script.getAttribute("mscursor-circle-outline") !== "disable") {
+            document
+              .querySelector("div.new")
+              .classList.add("mscursor-scale-outline");
+
+            if (script.getAttribute("color") !== null) {
+              if (script.getAttribute("color-outline") !== null) {
                 document.querySelector(
                   "div.new"
-                ).style.border = `0.5px solid black`;
+                ).style.border = `0.5px solid ${script.getAttribute(
+                  "color-outline"
+                )}`;
               } else {
                 document.querySelector(
                   "div.new"
-                ).style.border = `0.5px solid white`;
+                ).style.border = `0.5px solid ${script.getAttribute("color")}`;
+              }
+            } else {
+              if (script.getAttribute("color-outline") !== null) {
+                document.querySelector(
+                  "div.new"
+                ).style.border = `0.5px solid ${script.getAttribute(
+                  "color-outline"
+                )}`;
+              } else {
+                if (script.getAttribute("difference") == "disable") {
+                  document.querySelector(
+                    "div.new"
+                  ).style.border = `0.5px solid black`;
+                } else {
+                  document.querySelector(
+                    "div.new"
+                  ).style.border = `0.5px solid white`;
+                }
               }
             }
           }
-        }
-        document.querySelectorAll("div.mscursor-circle").forEach((element) => {
-          element.classList.add("mscursor-scale");
-        });
-      }, 100);
-    };
+          document
+            .querySelectorAll("div.mscursor-circle")
+            .forEach((element) => {
+              element.classList.add("mscursor-scale");
+            });
+        }, 100);
+      };
 
-    document.onmousemove = moove;
-    document.ontouchmove = moove;
+      document.onmousemove = moove;
+      document.ontouchmove = moove;
+    }
   }
 });
